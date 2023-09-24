6f456a031a139ee032cf30aa399988f87adc89cb
unknown <jschueren1431@gmail.com>
2023-09-23 18:26:33 -0700

    added landing page and updated some styling for the home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,32 @@ import Homebox from "./Homebox";
 
 function Home(){
     fetch("https://learn-geo-api.onrender.com/");
-    return(<div>
+    return(<div className="home-container">
+        <div className="row home-row">
+            <div className="col-lg-6">
+                <div className="home-description">
+                    <h1 className="landing-header">Welcome to Learn Geo</h1>
+                    <p>Learn geo is an online learning tool to learn about the countries of the world.
+                        Learn Geo offers many activities to learn all about the countries through a study activity
+                        or a hands on learning activity. Learn Geo also offers activities you can play for fun,
+                        along with a mastery challenge. Click below to study or try out a random activity.
+                    </p>
+                    <div className='anchor-div'>
+                        <a className="study-anchor btn btn-dark">Study</a>
+                        <a className="random-anchor btn btn-outline-dark">Random</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-6">
+                <img  className="home-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Artistic_depiction_of_planet_Earth.jpg/861px-Artistic_depiction_of_planet_Earth.jpg?20201109194112"/>
+            </div>
+        </div>
         <h3 className="page-heading">Select An Activity</h3>
         <div className="container-fluid">
             <div className="row">
                 <div className="col-12 col-lg-6 col-md-12 col-sm-12">
                     <Homebox 
-                    size="big"
+                    size="big left"
                     link="/study"
                     code="FR"
                     title="Study" 
@@ -17,7 +36,7 @@ function Home(){
                 </div>
                 <div className="col-12 col-lg-6 col-md-12 col-sm-12">
                     <Homebox
-                    size="big"
+                    size="big right"
                     link="/higher-lower"
                     code="IN"
                     title="Higher or Lower"
